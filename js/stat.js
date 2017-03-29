90f3b3602b3155677b7be7debc4854af2efb0a8d
Evgeny Prudnikov <prudnikov.e.s@gmail.com>
2017-03-30 01:28:52 +0300

    Добавь цвета колонкам и выведи значения времени

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,6 @@
 
 // Good x position to align elements
 var x = 130;
-var myName = 'Вы';
 
 function renderStatistics(ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
@@ -24,20 +23,29 @@ function renderHistogram(ctx, names, times) {
   var barWidth = 40; // px
   var barMargin = 50; // px
   var myColor = 'rgba(255, 0, 0, 1)';
+  var myName = 'Вы';
 
   var maxTime = -1;
   for (var i = 0; i < times.length; i++) {
     maxTime = maxTime < times[i] ? times[i] : maxTime;
   }
 
-  // TODO round it
   var unit = histogramHeight / maxTime;
   for (i = 0; i < names.length; i++) {
-    var barHeight = times[i] * unit; 
-    ctx.fillStyle = myColor;
-    ctx.fillRect(x + i * (barWidth + barMargin), 235 - histogramHeight + (maxTime - times[i]) * unit, barWidth, barHeight); 
+    var barXPosition = x + i * (barWidth + barMargin);    
+    var barYPosition = 235 - histogramHeight + (maxTime - times[i]) * unit;    
+    var barHeight = times[i] * unit;
+
+    ctx.fillStyle = names[i] === myName ? myColor : generateRandomColor();
+    ctx.fillRect(barXPosition, barYPosition, barWidth, barHeight); 
 
     ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillText(names[i], x + i * (barWidth + barMargin), 250);
+    ctx.fillText(names[i], barXPosition, 250);
+    ctx.fillText(Math.round(times[i]), barXPosition, barYPosition - 5);
   }
+}
+
+function generateRandomColor() {
+  var alpha = Math.random() * 0.8 + 0.2; // 0.2 is min value to avoid completely transparent bar
+  return 'rgba(120, 120, 255, ' + alpha + ')';
 }
